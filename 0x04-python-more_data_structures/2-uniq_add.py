b48252e6a680dcd2098cf233ca875e29a4659b0e
John Macharia <machariajohn23@gmail.com>
2021-09-17 10:38:01 -0700

    Add all integers in a list(only once for each integer)

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list = set(my_list)
-    for i in range(len(my_list)):
-        sum += i
-    return(sum)
+    add = 0
+    for i in set(my_list):
+        add += i
+    return(add)
