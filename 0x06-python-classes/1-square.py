bab0b02e7a91096fbe7236e0edbb0d993cb1e9b8
John Macharia <machariajohn23@gmail.com>
2021-09-29 10:43:41 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''square class definition'''
+
+
 class square:
+    '''A square
+
+    Attributes:
+        __size (int): size of a side of a square
+    '''
     def __init__(self, size):
         '''initialize a square instance
 
