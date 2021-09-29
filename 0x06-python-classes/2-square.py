d4915712c520b6da33013eff3603b774c02078c7
John Macharia <machariajohn23@gmail.com>
2021-09-29 11:05:25 -0700

    Define a class with conditions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """A square
+
+    Attributes:
+        __size (int): size of a side of a square
+    """
+    def __init__(self, size=0):
+        """Initialize a square instance
+
+        Args:
+            size (int): size of the sides of a square
+
+        Returns: None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
