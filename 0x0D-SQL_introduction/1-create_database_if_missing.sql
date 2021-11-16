19eafbbb6e2850c0f5b28ede276ba7c3214c3b0e
John Macharia <machariajohn23@gmail.com>
2021-11-16 07:52:34 -0800

    Create a database and script should not fail if database already exists

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Script that creates a database in MYSQL server
+-- if the database already exists, the script should not fail
+-- Do not use the SELECT/SHOW statements
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
