b483936d0f233df73f7a00bf22ddd48603d23d9e
John Macharia <machariajohn23@gmail.com>
2022-02-08 11:43:57 -0800

    Function that adds 2 integers and uses doctests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,12 +4,12 @@
 
 def add_integer(a, b=98):
     """adds 2 integers"""
-    if type(a) == float:
+    if type(a) is float:
         a = int(a)
-    if type(b) == float:
+    if type(b) is float:
         b = int(b)
-    if type(a) != int and type(a) != float:
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    if type(b) != int and type(b) != float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
     return (a + b)
