ec8821477292a71f45f21d278cc05ab695029784
John Macharia <machariajohn23@gmail.com>
2022-02-08 11:33:28 -0800

    Function that adds 2 integers and uses doctests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-'''Function that adds 2 integers'''
+"""Module that adds 2 integers"""
 
-
-def add_integer(a, b):
-    '''return the addition of 2 integers'''
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type (b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
+def add_integer(a, b=98):
+    """adds 2 integers"""
+    if type(a) == float:
         a = int(a)
-    if type(b) is float:
+    if type(b) == float:
         b = int(b)
-    return(a + b)
+    if type(a) != int and type(a) != float:
+        raise TypeError('a must be an integer')
+    if type(b) != int and type (b) != float:
+        raise TypeError('b must be an integer')
+    return (a + b)
