8fde204d56c34402ed5796c138da2603b001cf59
John Macharia <machariajohn23@gmail.com>
2022-02-09 07:39:45 -0800

    Printing a square with '#', including doctests

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,9 +4,14 @@
 
 def print_square(size):
     '''prints a square'''
+
     if type(size) is not int:
-        raise TypeError("size must be an integer")
+        raise TypeError('size must be an integer')
     if size < 0:
-        raise ValueError("size must be >= 0")
-    if size > 0:
-        print(("#" * size + "\n") * size, end="")
+        raise ValueError('size must be >= 0')
+    if type(size) is float and size < 0:
+        raise TypeError('size must be an integer')
+    for col in range(size):
+        for row in range(size):
+            print("#", end="")
+        print()
