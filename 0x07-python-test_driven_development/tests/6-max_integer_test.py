7da61432c614de3117c1e19f88baa2086af3bf8b
John Macharia <machariajohn23@gmail.com>
2022-02-19 08:25:36 -0800

    Unittests for maximum value in a list

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,5 +15,5 @@ class print_max_integer(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4, -5]), 4)
 
     def test_types(self):
-        #self.assertRaises(TypeError, max_integer, 3+5j)
+        self.assertRaises(TypeError, max_integer, 3+5j)
         #self.assertRaises(TypeError, max_integer, "radius")
