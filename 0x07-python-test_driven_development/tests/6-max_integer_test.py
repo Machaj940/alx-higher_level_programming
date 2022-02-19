f3e17e51940bb655ba51b65bcc1eabce3ed8a1c7
John Macharia <machariajohn23@gmail.com>
2022-02-19 08:12:21 -0800

    Unittests for maximum value in a list

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class print_max_integer(unittest.TestCase):
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer(list()), None)
