e6340bbc5a6cc62ae1019f15e9b0b0bb637e90ad
John Macharia <machariajohn23@gmail.com>
2022-02-28 09:15:17 -0800

    Unittests for the Base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,4 +10,15 @@ class Testbase(unittest.TestCase):
     def test_id_is_not_none(self):
         b = Base(4)
         self.assertEqual(b.id, 4)
-        
+
+    def test_no_arg(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_negative_arg(self):
+        b = Base(-4)
+        self.assertEqual(b.id, -4)
+
+    def test_more_than_one_arg(self):
+        with self.assertRaises(TypeError):
+            b = Base(4, 4)
