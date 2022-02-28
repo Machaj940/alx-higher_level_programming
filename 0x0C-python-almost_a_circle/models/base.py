e6df214b5bb00173e276d1a66739ec7d43a0a2be
John Macharia <machariajohn23@gmail.com>
2022-02-28 09:00:23 -0800

    Base class for 'Almost-A-Circle'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,6 +9,6 @@ class Base:
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-        if id == None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
