ce0d546d5a87a06cbef5ce9b693d3aea864a27b4
John Macharia <machariajohn23@gmail.com>
2022-05-05 13:32:51 -0700

    Write a script that prints two arguments passed to it, in the following format: '--- is ---'

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+
+if (process.argv[2] && process.argv[3]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2]) {
+  console.log(process.argv[2] + ' is undefined');
+} else if (process.argv[3]) {
+  console.log('undefined is ' + process.argv[3]);
+} else {
+  console.log('undefined is undefined');
+}
