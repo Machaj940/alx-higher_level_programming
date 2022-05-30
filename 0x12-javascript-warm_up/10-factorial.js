faa352aa15c152d62a2f70c536b20a9d9b4ba597
John Macharia <machariajohn23@gmail.com>
2022-05-30 02:22:20 -0700

    Write a script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const process = require('process');
 
-
 if (isNaN(process.argv[2])) {
-    console.log(1);
+  console.log(1);
 } else if (process.argv[2] === 0) {
-    console.log(1);
+  console.log(1);
 } else {
-    let fact = 1;
-    for (i = 1; i <= process.argv[2]; i++) {
-        fact *= i;
-    }
-    console.log(fact);
+  let fact = 1;
+  for (let i = 1; i <= process.argv[2]; i++) {
+    fact *= i;
+  }
+  console.log(fact);
 }
