e62ea59fad82eda1e5285c4a0a170f0db177e6fa
John Macharia <machariajohn23@gmail.com>
2022-05-31 12:27:51 -0700

    Function that returns the number of occurrences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  const count = list.reduce((n, x) => n + (x === searchElement), 0);
-  console.log(count);
+  let numMatches = 0;
+    for (let i = 0, j = list.length; i < j; i += 1) {
+        if (list[i] === searchElement) {
+            numMatches += 1;
+        }
+    }
+    return numMatches;
 };
