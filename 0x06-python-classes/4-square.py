44e489f48974d46fe10654cb9f7197c4a4b76ed3
John Macharia <machariajohn23@gmail.com>
2023-03-25 16:20:44 +0300

    Write a class that includes property(getter) and property setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -50,3 +50,29 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+
+# OR
+
+#class Square:
+#    '''A class square'''
+#    def __init__(self, size=0):
+#       self.size = size
+
+#    @property
+#    def size(self):
+#        return self.__size
+
+#    @size.setter
+#    def size(self, value):
+#	if isinstance(value, int):
+#            self.__size = value
+#	else:
+#            raise TypeError("size must be an integer")
+#	if value >= 0:
+#            self.__size = value
+#        else:
+#            raise ValueError("size must be >= 0")
+
+#    def area(self):
+#        return self.__size * self.__size
