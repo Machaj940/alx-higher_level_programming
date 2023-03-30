5b141c9d40b88b7f68d259d7e5c3fdcbe74ad10c
John Macharia <machariajohn23@gmail.com>
2023-03-30 22:03:25 +0300

    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""function that prints a text with 2 new lines after each of these 
-   characters: ., ? and :"""
+"""function that prints a text with 2 new lines after '.', '?' and ':'"""
 
 def text_indentation(text):
     """the function"""
