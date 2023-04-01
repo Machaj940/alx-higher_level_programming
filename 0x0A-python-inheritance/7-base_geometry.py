7738b2a193a11a4369ee0a0795332c961c989f4c
John Macharia <machariajohn23@gmail.com>
2023-04-01 19:20:57 +0300

    Testing a class that has exceptions with doctests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-'''Create an class BaseGeometry'''
+"""Class with exceptions and relevant Doctests"""
 
 
 class BaseGeometry:
-    '''a class'''
+    """the Class"""
     def area(self):
-        '''raise an exception'''
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        '''validates that value is an int greater than 0'''
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
