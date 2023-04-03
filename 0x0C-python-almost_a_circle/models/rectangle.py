56dc27485573ae4e63380e43f84cf69f535ff983
John Macharia <machariajohn23@gmail.com>
2023-04-03 20:37:08 +0300

    Print rectangle using '#'

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,3 +65,10 @@ class Rectangle(Base):
     def area(self):
         """return area"""
         return self.__width * self.__height
+
+    def display(self):
+        """print the rectangle with #"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
