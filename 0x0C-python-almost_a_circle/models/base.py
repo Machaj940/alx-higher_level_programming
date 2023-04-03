61264e1d89ded03a78e164dc9bbbe4c5ad55655c
John Macharia <machariajohn23@gmail.com>
2023-04-04 00:36:51 +0300

    Add static method that returns the JSON string representation of a list ofdictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,15 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return f"[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        listofDictionaries = list(map(cls.to_dictionary, list_objs))
+        dictlist_to_json = cls.to_json_string(listofDictionaries)
+        with open(f"{cls.__name__}.json", mode="w",
+                  encoding="utf-8") as f:
+            f.write(dictlist_to_json)
+
+# OR
+# json.dump(listofDictionaries, f)
+# you do not need to do the dictlist_to_json... step like above
