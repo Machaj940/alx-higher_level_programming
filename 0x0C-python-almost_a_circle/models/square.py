13db856fdac9295c1ef6f93bb6b674437cb53b3e
John Macharia <machariajohn23@gmail.com>
2023-04-03 21:40:53 +0300

    class Square inherits from Rectangle, including changing width and height instance attributes to size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,11 +5,14 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """inherits from class Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
+        """initialize instance attributes"""
         self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Print the square details"""
         return (
             f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - "
             f"{self.size}"
