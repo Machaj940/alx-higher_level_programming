6ff1a3674fe9edd718a86cd565c6a4b8fe4c6768
John Macharia <machariajohn23@gmail.com>
2023-04-03 19:42:59 +0300

    class Rectangle inherits instance variable from Base class using super()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,55 +2,42 @@
 '''A Rectangle class'''
 
 
-from base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
     ''' A rectangle class '''
     def __init__(self, width, height, x=0, y=0, id=None):
-        ''' initialize the rectangle '''
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
-        ''' returning private attribute '''
         return self.__width
-
     @width.setter
     def width(self, value):
-        ''' setting private attribute '''
         self.__width = value
 
     @property
     def height(self):
-        ''' returning private attribute '''
         return self.__height
-
     @height.setter
     def height(self, value):
-        ''' setting private attribute '''
         self.__height = value
 
     @property
     def x(self):
-        ''' returning private attribute '''
         return self.__x
-
     @x.setter
     def x(self, value):
-        ''' setting private attribute '''
         self.__x = value
 
     @property
     def y(self):
-        ''' returning private attribute '''
         return self.__y
-
-    @x.setter
+    @y.setter
     def y(self, value):
-        ''' setting private attribute '''
         self.__y = value
