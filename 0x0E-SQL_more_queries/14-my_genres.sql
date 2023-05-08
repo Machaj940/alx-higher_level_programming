7e56196abe834f70c99bd22bf495030b100bbaae
John Macharia <machariajohn23@gmail.com>
2023-05-08 20:30:42 +0300

    List all genres of the show Dexter by JOINING all 3 tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,8 +5,5 @@ FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.genre_id = tv_shows.id
-WHERE tv_show_genres.show_id = (
-SELECT tv_shows.id
-FROM tv_shows
-WhERE tv_shows.title = "Dexter")
+WHERE tv_show_genres.show_id = 8
 ORDER BY tv_genres.name;
