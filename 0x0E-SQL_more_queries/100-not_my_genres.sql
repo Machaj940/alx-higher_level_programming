da7ea76bed59cb9a796f36de88f0bd9bf1fc904b
John Macharia <machariajohn23@gmail.com>
2023-05-08 23:25:59 +0300

    List all genres not linked to the show Dexter

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,15 @@
+-- List all genres not linked to the show Dexter
+
+-- List all genres of the show Dexter
+
+SELECT tv_genres.name
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id NOT IN (
+SELECT tv_show_genres.genre_id
+FROM tv_show_genres
+WHERE show_id = 8)
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name;
