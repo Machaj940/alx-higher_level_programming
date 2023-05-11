bc1f731073e5de52563300c6bc7e922652f08222
John Macharia <machariajohn23@gmail.com>
2023-05-12 00:32:32 +0300

    Add the first 2 args passed to the file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 
-function add(a, b) {
-    console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 
 add(Number(process.argv[2]), Number(process.argv[3]));
