787b69085305e7afc6cf9fe3acdb453906307021
John Macharia <machariajohn23@gmail.com>
2023-05-11 23:52:07 +0300

    Write a script that prints 'C is fun' the number of times the arg given after the file in terminal

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (!isNaN(parseInt(process.argv[2]))) {
-  for (let i = 0; i < process.argv[2]; i++) {
-    console.log('C is fun');
-  }
+const str = 'C is fun';
+
+const x = Number(process.argv[2]);
+
+if (x) {
+    for (i = 0; i < x; i++) {
+        console.log(str);
+    }
 } else {
-  console.log('Missing number of occurrences');
+    console.log('Missing number of occurrences');
 }
