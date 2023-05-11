5930d5e302193433666d055defdd4bb336b0258a
John Macharia <machariajohn23@gmail.com>
2023-05-12 01:28:54 +0300

    Find the second largest number of the command line arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,9 @@ const process = require('process');
 const argArray = [...new Set(process.argv)];
 
 if (argArray) {
-    newArray = argArray.slice(2);
-    result = newArray.sort(function(a, b) { return b - a})[1];
-    console.log(result);
-}
-else {
-    console.log(0);
+  const newArray = argArray.slice(2);
+  const result = newArray.sort(function (a, b) { return b - a; })[1];
+  console.log(result);
+} else {
+  console.log(0);
 }
