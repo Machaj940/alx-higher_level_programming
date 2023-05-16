421796346f5f78fa3e5805e23e58d16ab982bada
John Macharia <machariajohn23@gmail.com>
2023-05-16 20:38:05 +0300

    Write a python script that lists all states from the database hbtn_0e_0_usa using MySQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,13 +8,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306, charset="utf8")
     '''make a connection to the database that you wish to use'''
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
