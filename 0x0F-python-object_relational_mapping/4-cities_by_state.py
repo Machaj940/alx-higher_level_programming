701fd7de30a6cf3a62d2921da7d91d32bd80b0e7
John Macharia <machariajohn23@gmail.com>
2023-05-16 22:22:26 +0300

    Join tables using MySQLdb

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name  FROM states \
-                 INNER JOIN cities WHERE states.id = cities.state_id \
+                 INNER JOIN cities ON states.id = cities.state_id \
                  ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
