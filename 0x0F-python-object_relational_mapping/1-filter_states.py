05e2c3f7b2c5bda278571d1b0b18abf73cd796a1
John Macharia <machariajohn23@gmail.com>
2023-05-16 20:53:30 +0300

    Write a script that lists all states with a name starting with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1][0] == 'N':
-            print(row)
+        print(row)
     cur.close()
     db.close()
