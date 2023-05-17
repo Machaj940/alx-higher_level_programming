146925efaf4993b99c47624d43d8e95e403bde2b
John Macharia <machariajohn23@gmail.com>
2023-05-17 19:48:15 +0300

    Contains the class definition of a city. Related to states by a foreign key

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Write a python file that contains the class definition of a City
+   and an instance Base = declarative_base()
+'''
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    '''A city'''
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id= Column(Integer, ForeignKey('states.id'), nullable=False)
