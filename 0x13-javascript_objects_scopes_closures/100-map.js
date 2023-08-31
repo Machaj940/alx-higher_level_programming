084f1fe2eee2b9d4be458609c9c1cbea339e1bbd
John Macharia <machariajohn23@gmail.com>
2023-08-31 21:25:47 +0300

    Create new array with each value equal to the value of the initial list, multipled by the index in the list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -5,3 +5,6 @@ const list = require('./100-data.js').list;
 const newArray = list.map((element, index) => element * index);
 console.log(list);
 console.log(newArray);
+
+//const newList = list.map(item => item * list.indexOf(item));
+//console.log(newList);
