a13f1dccd2a239230ba3e1d821f82bba7a8332fd
John Macharia <machariajohn23@gmail.com>
2023-09-02 01:13:47 +0300

    Script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
+const process = require('process');
 const fs = require('fs');
+
 const url = process.argv[2];
-const filePath = process.argv[3];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   }
+  fs.writeFile(process.argv[3], body, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
