f6fa5e9a9e953f84d823e04130cc98003e5ec1ff
John Macharia <machariajohn23@gmail.com>
2024-03-23 02:43:09 +0300

    Write a script that prints a square. The first argument is the size of the square. If the first argument can't be converted to an integer, print 'Missing size'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,14 @@
 
 const process = require('process');
 
-if (typeof parseInt(process.argv[2], 10) === "number") {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-	let row = ''
-	for (let j = 0; j < parseInt(process.argv[2]); j++) {
-	    row += "X";
-	}
-	console.log(row + " ");
-    }
-} else {
+if (isNaN(parseInt(process.argv[2]))) {
     console.log("Missing size");
+} else {
+    for (let i = 0; i < parseInt(process.argv[2]); i++) {
+        let row = ''
+        for (let j = 0; j < parseInt(process.argv[2]); j++) {
+            row += "X";
+        }
+        console.log(row + " ");
+    }
 };
