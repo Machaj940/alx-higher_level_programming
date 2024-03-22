7bf1747dc0a9c8a67223b92094cdef5c8d77e5e4
John Macharia <machariajohn23@gmail.com>
2024-03-23 02:08:41 +0300

    Write a script that prints the first argument passed to it. If no arguments are passed to the script, print 'No argument'

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+'use strict';
+
 const process = require('process');
 
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
+if (!process.argv[2]) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
